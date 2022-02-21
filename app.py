b7302b5c14a4c73a4faacf5d7c4d2ab1cbc45a73
guanxianghui <1908461240@qq.com>
2022-02-21 15:47:11 +0800

    404 page

--- app.py
+++ app.py
@@ -2,6 +2,20 @@ import sys
 from flask import Flask,render_template
 import os
 from flask_sqlalchemy import SQLAlchemy
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 WIN=sys.platform.startswith('win')
 if WIN:
     prefix='sqlite:///'
